a2363baa10375e650c7580c7aaf21490e7365689
Kunal Pathak <69764621+kunalpa@users.noreply.github.com>
2022-02-26 22:10:41 -0500

    Edits

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import * as coins from './modules/coin.mjs'
 import { createRequire } from 'module'
 const require = createRequire(import.meta.url)
 
+// Require Express.js
 const express = require('express')
 const app = express()
 
@@ -10,19 +11,19 @@ const args = require('minimist')(process.argv.slice(2))
 args['port']
 const port = args.port || process.env.PORT || 5000
 
-const server = app.listen(port, () => {
-    console.log('App listening on port %PORT%'.replace('%PORT%', port))
-})
-
+// Start an app server
+const server = app.listen(HTTP_PORT, () => {
+    console.log('App listening on port %PORT%'.replace('%PORT%',HTTP_PORT))
+});
 app.get('/app/', (req, res) => {
     // Responds with status 200
-    res.statusCode = 200
+    res.statusCode = 200;
     // Responds with status message "OK"
-    res.statusMessage = 'OK'
+    res.statusMessage = 'OK';
     res.writeHead(res.statusCode, {
         'Content-Type' : 'text/plain'
     });
-    res.end(res.statusCode + ' ' + res.statusMessage)
+    res.end(res.statusCode + ' ' + res.statusMessage);
 });
 
 app.get('/app/flip', (req, res) => {
@@ -30,7 +31,7 @@ app.get('/app/flip', (req, res) => {
 });
 
 app.get('/app/flips/:number', (req, res) => {
-    const flips = coins.coinFlips(req.params.number)
+    const flips = coins.coinFlips(req.params.number);
     const count = coins.countFlips(flips);
     res.status(200).json({ raw: flips, summary: count })
 });
@@ -39,6 +40,7 @@ app.get('/app/flip/call/:call(heads|tails)', (req, res) => {
     res.status(200).json(coins.flipACoin(req.params.call))
 })
 
+// Default response for any other request
 app.use(function(req, res){
     res.status(404).send('404 NOT FOUND')
 });
