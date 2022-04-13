9ba50ea3503ff45230fa1018e76146074146badf
Kunal Pathak <69764621+kunalpa@users.noreply.github.com>
2022-04-12 23:03:09 -0400

    inflation

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.get('/app/flips/:number', (req, res) => { // Flip a coin multiple times and 
     // Respond with status 200
     res.statusCode = 200;
     // Set up variable for number of coin flips, array of results, and counted results
-	const flips = req.params.number;
+	const flips = req.params.number; 
     const results = coinFlips(flips);
     const counted = countFlips(coinFlips(flips));
 	// Flip coin flips n times using the coinFlips function, send json response of results
