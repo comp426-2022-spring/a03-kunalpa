0295908887f5fdce11c94ded9917f05b8140d3b3
Kunal Pathak <69764621+kunalpa@users.noreply.github.com>
2022-04-12 23:00:27 -0400

    inflation

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const server = app.listen(port, () => {
 }); 
 
 app.get('/app/', (req, res) => { // Define Checkpoint 
-    // Respond with status 200 
+    // Respond with status 200  
     res.statusCode = 200; 
     // Respond with status message "OK" 
     res.statusMessage = 'OK';  
