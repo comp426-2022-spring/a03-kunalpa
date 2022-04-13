0d8b06d66f0b7d1e650b20008c884dda15b45041
Kunal Pathak <69764621+kunalpa@users.noreply.github.com>
2022-04-12 23:23:57 -0400

    inflation

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 import { coinFlip, coinFlips, countFlips, flipACoin } from './modules/coin.mjs';
 import { createRequire } from 'module';
-
 // Require Express.js
 const require = createRequire(import.meta.url)
 const express = require('express') 
