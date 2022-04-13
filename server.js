0b905bed67affe122c22ada9ede1e56546164a65
Kunal Pathak <69764621+kunalpa@users.noreply.github.com>
2022-04-12 23:14:03 -0400

    inflation

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ app.get('/app/flips/:number', (req, res) => {  // Flip a coin multiple times and
 app.get('/app/flip/call/heads', (req, res) => {  // Flip a coin, call heads, compare result
     // Respond with status 200 
     res.statusCode = 200; 
-       // Use flipACoin function, send json response of results 
+    // Use flipACoin function, send json response of results 
     res.json(flipACoin('heads'));
 });
  
