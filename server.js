1b51455f41f9621e14e191b79ebea265c605e7d5
Kunal Pathak <69764621+kunalpa@users.noreply.github.com>
2022-04-12 23:04:25 -0400

    inflation

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.get('/app/', (req, res) => { // Define Checkpoint
 app.get('/app/flip/', (req, res) => { // Flip a coin and return the result 
     // Respond with status 200 
     res.statusCode = 200; 
-    // Flip a coin using the coinFlip() function
+    // Flip a coin using the coinFlip() function 
     const result = coinFlip();
     // Send json response based of heads or tails from resulting coin flip
     if (result == 'heads') { 
